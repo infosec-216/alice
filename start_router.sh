47ed9f12078c2d2a4a939e168b5fdcf68e8ea486
Thar0l <gusarov.aa@gmail.com>
2015-11-13 20:32:14 +0000

    Fixed router start script.

--- start_router.sh
+++ start_router.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-iptables -t nat -S
-#iptables -t nat -D
-
-export ROUTER=192.168.0.135
+export ROUTER=192.168.0.111
 export SLEUTH=192.168.0.127
-export SUSPECT=192.168.0.120
+export SUSPECT=192.168.0.104
 export REAL_ROUTER=192.168.0.1
-
-ip rule list
-#ip rule list del
-ip route list table 111
 export TABLE=111
+export INTERFACE=eth0
+
+
+echo "CURRENT PARAMS:" 
+echo
+echo "IP RULES"
+ip rule list
+echo 
+echo "IP ROUTES"
+ip route list
+echo 
+echo "IP ROUTE TABLE:" $TABLE
+ip route list table $TABLE
+echo
+echo
+echo
 
 ip rule add from $SUSPECT table $TABLE priority 4
-#ip route del table 111
 ip route add default via $SLEUTH table $TABLE
-
-#iptables -t nat -I PREROUTING -s $SUSPECT -d $ROUTER -j DNAT --to-destination $SLEUTH
-
-iptables -t nat -S
-
-export INTERFACE=eth0
+echo
+echo
+echo "NEW PARAMS:" 
+echo
+echo "IP RULES"
+ip rule list
+echo 
+echo "IP ROUTES"
+ip route list
+echo 
+echo "IP ROUTE TABLE:" $TABLE
+ip route list table $TABLE
+echo
+echo
+echo
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
