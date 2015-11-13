4b87c00ceeed4ce1b1c4d203b0932fc7d7f92b24
Thar0l <gusarov.aa@gmail.com>
2015-11-13 21:07:53 +0000

    Added united sleught scripts file

---
+++ sleuth_script.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+export ROUTER=192.168.0.111
+export SLEUTH=192.168.0.127
+export SUSPECT=192.168.0.104
+export REAL_ROUTER=192.168.0.1
+export TABLE=111
+export INTERFACE=eth0
+
+
+
+if [ "$1" = "start" ]
+then
+	echo "CURRENT PARAMS:" 
+	echo
+	echo "IPTABLES"
+	iptables -S
+	echo "NAT"
+	iptables -t nat -S
+	echo 
+	echo
+	echo
+	iptables -I FORWARD -s $SUSPECT -j ACCEPT
+	iptables -I FORWARD -d $SUSPECT -j ACCEPT
+	iptables -t nat -I POSTROUTING -s $SUSPECT -j MASQUERADE
+	echo 1 > /proc/sys/net/ipv4/ip_forward
+	echo
+	echo
+	echo "NEW PARAMS:" 
+	echo
+	echo "IPTABLES"
+	iptables -S
+	echo "NAT"
+	iptables -t nat -S
+	echo 
+	echo
+	echo
+else
+if [ "$1" = "kill" ]
+then
+	echo "CURRENT PARAMS:" 
+	echo
+	echo "IPTABLES"
+	iptables -S
+	echo "NAT"
+	iptables -t nat -S
+	echo 
+	echo
+	echo
+	iptables -D FORWARD -d $SUSPECT -j ACCEPT
+	iptables -D FORWARD -s $SUSPECT -j ACCEPT
+	iptables -t nat -D POSTROUTING -s $SUSPECT -j MASQUERADE
+	echo 
+	echo 
+	echo 
+	echo "NEW PARAMS:" 
+	echo
+	echo "IPTABLES"
+	iptables -S
+	echo "NAT"
+	iptables -t nat -S
+	echo
+	echo 
+	echo
+else
+	echo "Please use 'start' or 'kill' args"
+fi
+fi
+
+
