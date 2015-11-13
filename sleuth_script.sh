5ed385b7b0b4305c23fe21c1097936cca731ed19
Thar0l <gusarov.aa@gmail.com>
2015-11-13 21:15:27 +0000

    Added forwarding for mitmproxy to sleuth script

--- sleuth_script.sh
+++ sleuth_script.sh
@@ -23,6 +23,8 @@ then
 	iptables -I FORWARD -s $SUSPECT -j ACCEPT
 	iptables -I FORWARD -d $SUSPECT -j ACCEPT
 	iptables -t nat -I POSTROUTING -s $SUSPECT -j MASQUERADE
+	iptables -t nat -A PREROUTING -i $INTERFACE -p tcp --dport 80 -j REDIRECT --to-port 8080 
+	iptables -t nat -A PREROUTING -i $INTERFACE -p tcp --dport 443 -j REDIRECT --to-port 8080	
 	echo 1 > /proc/sys/net/ipv4/ip_forward
 	echo
 	echo
@@ -50,7 +52,9 @@ then
 	iptables -D FORWARD -d $SUSPECT -j ACCEPT
 	iptables -D FORWARD -s $SUSPECT -j ACCEPT
 	iptables -t nat -D POSTROUTING -s $SUSPECT -j MASQUERADE
-	echo 0 > /proc/sys/net/ipv4/ip_forward
+	iptables -t nat -D PREROUTING -i $INTERFACE -p tcp --dport 80 -j REDIRECT --to-port 8080 
+	iptables -t nat -D PREROUTING -i $INTERFACE -p tcp --dport 443 -j REDIRECT --to-port 8080	
+	iecho 0 > /proc/sys/net/ipv4/ip_forward
 	echo 
 	echo 
 	echo 
